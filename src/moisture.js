6c9bbb0630a305e2285a6f679fd864d751c7fdf4
John McKim <johncmckim@gmail.com>
2016-08-14 22:41:08 +1000

    Show example event

--- src/moisture.js
+++ src/moisture.js
@@ -7,6 +7,9 @@ const NotifyService = require('./services/notify')
 const sns = new AWS.SNS();
 const notify = NotifyService({ sns: sns });
 
+// Example event
+// { DeviceId: 'test-js-device', Recorded: '2016-08-14T12:39:06.765Z', Level: 3.59334681998007 }
+
 module.exports.checkLevel = (event, context, cb) => {
   console.log(event);
   cb(null, { message: 'success' });
