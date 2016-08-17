f2adace7ef563418f0557a44ec5b5f7a1b7808fa
John McKim <johncmckim@gmail.com>
2016-08-18 07:49:30 +1000

    Add iopipe support

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
-const config = require('./config.json');
+const env = require('./env');
 
-require('./env')(config);
+const config = require('./config.json');
+const secrets = require('./secrets.json');
+
+env(config);
+env(secrets, true);
 
 // Setup env vars before requiring functions
 const moisture = require('./src/moisture');
+const iopipe = require('iopipe')({
+   clientId: process.env.iopipe.key
+});
 
-module.exports.checkMoistureLevel = moisture.checkLevel;
+module.exports.checkMoistureLevel = iopipe(moisture.checkLevel);
